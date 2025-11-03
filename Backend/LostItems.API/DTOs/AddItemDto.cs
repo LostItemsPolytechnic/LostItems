9d6e9633a7a8c5982a7e248b37556151f9214c49
Nazar Tsventarnyi <youtopak1@gmail.com>
2025-11-03 12:50:48 +0200

    Update AddItemDto.cs

--- Backend/LostItems.API/DTOs/AddItemDto.cs
+++ Backend/LostItems.API/DTOs/AddItemDto.cs
@@ -2,7 +2,7 @@
 {
     public class AddItemDto
     {
-        public string dlgkjdfgkj { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
     }
 }
